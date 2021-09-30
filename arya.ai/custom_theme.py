a59720aa6adbca5edbfa2f4f32d070f603425315
vyaduvanshi <vikram.yaduvanshi13@gmail.com>
2021-09-30 19:08:12 +0530

    adding assignment

---
+++ arya.ai/custom_theme.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:58e4e624a74010abe84d66c993752af12c057a1f398158b5e8984316b3250ebb
+size 15010
---
+++ arya.ai/script_question3.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ac04b04ffb74be366710486683c3309099a0e543813ec467e668f2a1468307c0
+size 3598
